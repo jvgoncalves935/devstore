6e7ce757d956d1715faf5d5afff089afc1ef4a85
João Vítor Gonçalves <jvgoncalves935@hotmail.com>
2024-05-24 14:58:02 -0300

    Env variables boundary

--- src/env.ts
+++ src/env.ts
@@ -1,16 +1,15 @@
-import { z } from 'zod'
+import { createEnv } from "@t3-oss/env-nextjs"
+import { z } from "zod"
 
-const envSchema = z.object({
+export const env = createEnv({
+  server: {
+    APP_URL: z.string().url(),
+  },
+  client: {
     NEXT_PUBLIC_API_BASE_URL: z.string().url(),
-    APP_URL: z.string().url()
-})
-
-const parsedEnv = envSchema.safeParse(process.env)
-
-if(!parsedEnv.success){
-    console.error('Invalid enviroment variables',parsedEnv.error.flatten().fieldErrors)
-
-    throw new Error('Invalid enviroment variables.')
-}
-
-export const env = parsedEnv.data
+  },
+  runtimeEnv: {
+    APP_URL: process.env.APP_URL,
+    NEXT_PUBLIC_API_BASE_URL: process.env.NEXT_PUBLIC_API_BASE_URL,
+  },
+});
