472806c8f40389ee6b653d72fbccac084b975aef
João Vítor Gonçalves <jvgoncalves935@hotmail.com>
2024-05-24 14:33:25 -0300

    Suspense getSearchParams

--- src/app/(store)/layout.tsx
+++ src/app/(store)/layout.tsx
@@ -7,7 +7,7 @@ export default function StoreLayout({ children }: { children: ReactNode }){
         <CartProvider>
             <div className="mx-auto grid min-h-screen w-full max-w-[1600px] grid-rows-app gap-5 p-8">
                 <Header />
-                <Suspense>
+                <Suspense fallback={null}>
                     {children}
                 </Suspense>
             </div>
--- src/components/header.tsx
+++ src/components/header.tsx
@@ -13,7 +13,7 @@ export function Header(){
                     devstore
                 </Link>
                 
-                <Suspense>
+                <Suspense fallback={null}>
                     <SearchForm />
                 </Suspense>
                 
